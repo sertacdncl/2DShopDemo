bbe97d44cab9b98ab34a02cc6d36d0f5f24f2029
SertacDincel <sertac.dincel@bytelab.com.tr>
2025-07-10 17:56:40 +0300

    Lua player movement with animation added

---
+++ Assets/_Game/Scripts/Lua/Resources/Lua/player.lua
@@ -0,0 +1,52 @@
+﻿---
+--- Created by BYTE.
+--- DateTime: 10.07.2025 14:27
+---
+
+local Player = {}
+Player.__index = Player
+
+Player.position = CS.UnityEngine.Vector2(0, 0)
+Player.moveSpeed = 5
+Player.gameObject = nil
+Player.animator = nil
+Player._lookDirection = nil
+
+Player._dirXHash = CS.UnityEngine.Animator.StringToHash("DirX")
+Player._dirYHash = CS.UnityEngine.Animator.StringToHash("DirY")
+Player._speedHash = CS.UnityEngine.Animator.StringToHash("Speed")
+
+function Player.update(dt, input)
+    if not Player.rb then return end
+    if input == nil then return end
+
+    local move = input * Player.moveSpeed * dt
+    local movement = move * Player.moveSpeed
+    local movementSpeed = movement.magnitude
+    Player.position = Player.position + move
+    SetLookDirection(input)
+    Player.rb:MovePosition(Player.position)
+    Player.animator:SetFloat(Player._dirXHash, Player._lookDirection.x)
+    Player.animator:SetFloat(Player._dirYHash, Player._lookDirection.y)
+    Player.animator:SetFloat(Player._speedHash, movementSpeed)
+end
+
+function SetLookDirection(direction)
+    if not Player.animator then return end
+
+    if math.abs(direction.x) > math.abs(direction.y) then
+        if direction.x > 0 then
+            Player._lookDirection = CS.UnityEngine.Vector2.right
+        else
+            Player._lookDirection = CS.UnityEngine.Vector2.left
+        end
+    else
+        if direction.y > 0 then
+            Player._lookDirection = CS.UnityEngine.Vector2.up
+        else
+            Player._lookDirection = CS.UnityEngine.Vector2.down
+        end
+    end
+end
+
+return Player
---
+++ Assets/_Game/Scripts/Player/PlayerControllerLua.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using XLua;
+
+[CSharpCallLua]
+public delegate void LuaPlayerUpdate(float deltaTime, Vector2 joystickInput);
+
+public class PlayerControllerLua : MonoBehaviour
+{
+	[SerializeField] private Animator animator;
+
+	private LuaTable _playerLua;
+	private Vector2 _joystickInput;
+
+	private void Start()
+	{
+		LuaEnv luaenv = new LuaEnv();
+		var luaScript = Resources.Load<TextAsset>("Lua/player");
+		_playerLua = luaenv.DoString(luaScript.text, "player")[0] as LuaTable;
+		
+		if(_playerLua == null)
+		{
+			Debug.LogError("Failed to load Player Lua script.");
+			return;
+		}
+		
+		JoystickEvents.OnMove += dir => _joystickInput = dir;
+		JoystickEvents.OnEnd += () => _joystickInput = Vector2.zero;
+		
+		_playerLua.Set("rb", GetComponent<Rigidbody2D>());
+		_playerLua.Set("position", (Vector2)transform.position);
+		_playerLua.Set("animator", animator);
+	}
+
+	private void FixedUpdate()
+	{
+		_playerLua.Get<LuaPlayerUpdate>("update")?.Invoke(Time.deltaTime, _joystickInput);
+	}
+}
